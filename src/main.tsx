893c8ab0039f6e6291e1d8f48134ffce9883c362
VuTuan200303 <vutuan200303@gmail.com>
2023-07-17 23:31:49 +0700

    ...

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ProductProvider } from "./context/Product.tsx";
+import CounterProvider from "./context/Counter.tsx";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <ProductProvider>
+        <CounterProvider>
+            <App />
+        </CounterProvider>
+    </ProductProvider>
+);
