c5669fc9eb37c9738ce40cfa7a9a3a5897d3b511
VuTuan200303 <vutuan200303@gmail.com>
2023-07-18 10:43:22 +0700

    init

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import App from "./App.tsx";
 import "./index.css";
 import { ProductProvider } from "./context/Product.tsx";
 import CounterProvider from "./context/Counter.tsx";
-// import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <ProductProvider>
