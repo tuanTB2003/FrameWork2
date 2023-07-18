8f6f3e5f135965b3cc7aea7a357674bf5f4ff835
VuTuan200303 <vutuan200303@gmail.com>
2023-07-18 11:03:19 +0700

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ProductProvider } from "./context/Product.tsx";
-import CounterProvider from "./context/Counter.tsx";
-
+import store from "./app/store.ts";
+// import { ProductProvider } from "./context/Product.tsx";
+// import CounterProvider from "./context/Counter.tsx";
+import { Provider } from "react-redux";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <ProductProvider>
-        <CounterProvider>
-            <App />
-        </CounterProvider>
-    </ProductProvider>
+    // <ProductProvider>
+    //     <CounterProvider>
+    //         <App />
+    //     </CounterProvider>
+    // </ProductProvider>
+    <Provider store={store}>
+        <App></App>
+    </Provider>
 );
